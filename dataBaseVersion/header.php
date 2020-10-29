2a89f7cfdaa05734290d4aa2dd8c427f82958356
simonlindstedt <70521702+simonlindstedt@users.noreply.github.com>
2020-10-29 07:27:20 +0100

    added normalize.css

--- dataBaseVersion/header.php
+++ dataBaseVersion/header.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Real Bible News</title>
+  <link rel="stylesheet" href="normalize.css">
   <link rel="stylesheet" href="main.css">
 </head>
 
