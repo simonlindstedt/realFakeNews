a377f993c8c26bbc5309708113b4c0a7e2bd085b
simonlindstedt <70521702+simonlindstedt@users.noreply.github.com>
2020-10-30 15:05:27 +0100

    fixed a typo

--- dataBaseVersion/database.php
+++ dataBaseVersion/database.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 $dsn = 'sqlite:' . __DIR__ . '/testFakeNewsDB.sqlite';
 
-//Borrowd these from https://phpdelusions.net/pdo
+//Borrowed these from https://phpdelusions.net/pdo
 $options = [
   PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
   PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
